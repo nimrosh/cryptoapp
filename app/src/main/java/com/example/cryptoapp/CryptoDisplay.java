af8402e146aa23efa1376245848eda7d83526d58
nimrosh <rmerugu2017@fau.edu>
2021-12-04 13:25:16 -0500

    fragments added to navigation bar items

--- app/src/main/java/com/example/cryptoapp/CryptoDisplay.java
+++ app/src/main/java/com/example/cryptoapp/CryptoDisplay.java
@@ -1,14 +1,27 @@
 package com.example.cryptoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class CryptoDisplay extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class CryptoDisplay extends AppCompatActivity {
+    BottomNavigationView navView;
+    NavController navController;
+    NavHostFragment navHostFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crypto_display);
+
+        navView = (BottomNavigationView) findViewById(R.id.top_navigation);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
